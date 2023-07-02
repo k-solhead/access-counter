fb91a5e322d748189deec5b708f9c0a7faf4920e
k-solhead <hyodo.ka@gmail.com>
2023-07-02 19:20:45 +0900

    add css

--- index.php
+++ index.php
@@ -21,14 +21,18 @@ $file = file('access.log');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>アクセスカウンター</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <h1>アクセスカウンター</h1>
-  <div class="counter-area">
-    <span class="counter"><?php $count = count($file);echo 'あなたは'.$count.'人目の訪問者です。';?></span>
-  </div>
+  <div class="wrapper">
+    <h1>アクセスカウンター</h1>
+    <div>
+      <h2><?php $count = count($file);echo 'あなたは<span class="counter">'.($count-1).'</span>人目の訪問者です。';?></h2>
+    </div>
     <?php foreach($file as $row) { ?>
     <p><?php echo $row;?></p>
     <?php }?>
+  </div>
 </body>
 </html>
